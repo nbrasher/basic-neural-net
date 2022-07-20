65662b6063b7ff5ef307c029b763d5f5ef6e32e5
Nathan Brasher <nathanbrasher@Annelises-Brasher-MacBook-Pro.local>
2022-07-20 16:40:00 -0500

    feat: new flatten layer

--- nn/layers.py
+++ nn/layers.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from math import prod
+from typing import Tuple
 
 import numpy as np
 
@@ -6,14 +8,6 @@ from nn.activation import relu, relu_deriv, sigmoid, sigmoid_deriv, softmax
 
 
 class Layer(ABC):
-    def __init__(self, input_shape: int, output_shape: int):
-        self.W = np.random.normal(
-            scale=np.sqrt(2.0 / (input_shape + output_shape)),
-            size=(output_shape, input_shape),
-        )
-        self.A = np.empty((input_shape, 1))
-        self.Z = np.empty((output_shape, 1))
-    
     @abstractmethod
     def forward_pass(self, x: np.ndarray) -> np.ndarray:
         pass
@@ -24,9 +18,15 @@ class Layer(ABC):
 
 
 class Dense(Layer):
-    """ Basic flat, dense neural network layer """
+    """Basic flat, dense neural network layer"""
+
     def __init__(self, input_shape: int, output_shape: int, activation: str):
-        super().__init__(input_shape, output_shape)
+        self.W = np.random.normal(
+            scale=np.sqrt(2.0 / (input_shape + output_shape)),
+            size=(output_shape, input_shape),
+        )
+        self.A = np.empty((input_shape, 1))
+        self.Z = np.empty((output_shape, 1))
 
         if activation == "relu":
             self.act = relu
@@ -34,17 +34,20 @@ class Dense(Layer):
         elif activation == "sigmoid":
             self.act = sigmoid
             self.act_deriv = sigmoid_deriv
+        elif activation == "softmax":
+            self.act = softmax
+            self.act_deriv = None  # The softmax gradient is more complex and unneeded
         else:
             raise ValueError(
                 "activation fn must be one of ['relu', 'sigmoid'],"
                 + f" but {activation} was passed"
             )
-    
+
     def forward_pass(self, x: np.ndarray) -> np.ndarray:
         self.A = x
         self.Z = self.W @ self.A
         return self.act(self.Z)
-    
+
     def backward_pass(self, x: np.ndarray, lr: float) -> np.ndarray:
         error = x * self.act_deriv(self.Z)
         ret = self.W.T @ error
@@ -53,16 +56,13 @@ class Dense(Layer):
         return ret
 
 
-class MultiClassLastLayer(Layer):
-    """ Basic flat final multi-class neural network layer.
-        Split this one out separately because of the loss function math.
+class MultiClassLastLayer(Dense):
+    """Basic flat final multi-class neural network layer.
+    Split this one out separately because of the loss function math.
     """
-    def __init__(self, input_shape: int, output_shape: int):
-        super().__init__(input_shape, output_shape)
 
-    def forward_pass(self, x: np.ndarray) -> np.ndarray:
-        self.A = x
-        return softmax(self.W @ self.A)
+    def __init__(self, input_shape: int, output_shape: int):
+        super().__init__(input_shape, output_shape, "softmax")
 
     def backward_pass(self, x: np.ndarray, lr: float) -> np.ndarray:
         error = x / x.shape[0]
@@ -70,3 +70,18 @@ class MultiClassLastLayer(Layer):
         self.W -= lr * np.outer(error, self.A)
 
         return ret
+
+
+class Flatten(Layer):
+    """Translation layer from a 2- or 3-D tensor to a column vector"""
+
+    def __init__(self, input_shape: Tuple[int]):
+        self.dim = len(input_shape)
+        self.input_shape = input_shape
+        self.output_shape = prod(input_shape)
+
+    def forward_pass(self, x: np.ndarray) -> np.ndarray:
+        return x.reshape((self.output_shape, 1))
+
+    def backward_pass(self, x: np.ndarray, lr: float) -> np.ndarray:
+        return x.reshape(self.input_shape)
--- nn/tests/test_layers.py
+++ nn/tests/test_layers.py
@@ -1,8 +1,8 @@
-from numpy.testing import assert_array_almost_equal
 import numpy as np
 import pytest
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
-from nn.layers import Dense
+from nn.layers import Dense, Flatten
 
 
 @pytest.fixture
@@ -10,35 +10,59 @@ def dense_layer():
     layer = Dense(input_shape=2, output_shape=3, activation="relu")
 
     # Replace weights for test purposes
-    layer.W = np.array([[1., 1.], [1., 0.], [-1., -1.]])
+    layer.W = np.array([[1.0, 1.0], [1.0, 0.0], [-1.0, -1.0]])
     yield layer
+
 
 def test_dense(dense_layer):
     assert dense_layer.W.shape == (3, 2)
     assert dense_layer.A.shape == (2, 1)
     assert dense_layer.Z.shape == (3, 1)
 
+
 def test_error():
     # Raises error on incorrect activation fn
     with pytest.raises(ValueError):
-        d = Dense(input_shape=2, output_shape=4, activation="foo")
+        Dense(input_shape=2, output_shape=4, activation="foo")
+
 
 def test_dense_fp(dense_layer):
-    input = np.array([[4.], [2.]])
+    input = np.array([[4.0], [2.0]])
     res = dense_layer.forward_pass(input)
     assert_array_almost_equal(dense_layer.A, input)
     assert_array_almost_equal(dense_layer.Z, np.array([[6], [4], [-6]]))
     assert_array_almost_equal(res, np.array([[6], [4], [0]]))
 
+
 def test_dense_bp(dense_layer):
-    dense_layer.forward_pass(np.array([[4.], [2.]]))
+    dense_layer.forward_pass(np.array([[4.0], [2.0]]))
 
-    feedback = np.array([[1.], [1.], [1.]])
+    feedback = np.array([[1.0], [1.0], [1.0]])
     original_weights = dense_layer.W.copy()
-    exp_correction = np.array([[.4, .2], [.4, .2], [0., 0.]])
-    exp_error = np.array([[2.], [1.]])
+    exp_correction = np.array([[0.4, 0.2], [0.4, 0.2], [0.0, 0.0]])
+    exp_error = np.array([[2.0], [1.0]])
 
-    res = dense_layer.backward_pass(feedback, .1)
+    res = dense_layer.backward_pass(feedback, 0.1)
 
     assert_array_almost_equal(dense_layer.W, original_weights - exp_correction)
     assert_array_almost_equal(res, exp_error)
+
+
+def test_flatten_2d():
+    flat = Flatten(input_shape=(2, 3))
+
+    input = np.array([[1, 2, 3], [4, 5, 6]])
+    exp = np.array([[1], [2], [3], [4], [5], [6]])
+
+    assert_array_equal(flat.forward_pass(input), exp)
+    assert_array_equal(flat.backward_pass(exp, 0.01), input)
+
+
+def test_flatten_3d():
+    flat = Flatten(input_shape=(2, 2, 3))
+
+    input = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+    exp = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12]])
+
+    assert_array_equal(flat.forward_pass(input), exp)
+    assert_array_equal(flat.backward_pass(exp, 0.01), input)
